c96d6152e0f872fa8a40a8856f724221b10c11c8
letizianeri <letizia.neri@mail.polimi.it>
2025-10-07 11:49:13 +0200

    funzioni di disegno (class 3)

--- class 2/sketch.js
+++ class 2/sketch.js
@@ -8,51 +8,37 @@ function setup() {
   createCanvas(xMax, yMax);
 }
 
-function draw() {
-  background(20,24,40);
-  // mostrare un testo bianco
-  // che dice le coordinate al mouse
-  //sul foglio da disegno
-  fill(255); //bianco
-  textSize(20);
-  text("mouseX: " + mouseX +  ",    mouseY: " + mouseY,20,20);  //stringa, x, y
-
-  // disegnare le stelle
-  // 120
-  // tre tipi di stelle: a, b, c
-  // fino a che abbiamo 120 stelle
-  // stelle ellipse
-  push();
-
-  // un unico ciclo
-  // creare una sequenza per fare a, b, c --> selezione (condizionale)
-  noStroke(); //toglie lo stroke a tutti gli elementi di questo contesto
-
-  for(let i=0; i < 120; i++){
-    let starX = ((i*37) % width) + (i%3) * 5;
-    let starY = ((i*73) % height) + (i%7);
-    // operatore modulo %
-    //stella a quando i è pari
-    if(i % 2 == 0){ //--> tutti i numeri pari divisi x 2 danno come resto 0
+function drawSingleStar(i, starX, starY, random_transparecy, random_size) {
+  if(i % 2 == 0){ //--> tutti i numeri pari divisi x 2 danno come resto 0
       // stella tipo a
-      fill(255,255,150);
-      ellipse(starX,starY,1);
+      fill(255,255,150,random_transparecy);
+      ellipse(starX,starY,random_size);
       //le stelle b quando i è divisibile x 3
-    }else if(i % 3 == 0) { //alla seconda iterazione i == 2
+    } else if(i % 3 == 0) { //alla seconda iterazione i == 2
       //stella tipo b
       fill(200,100,255);
       ellipse(starX,starY,1.5);
-    }else{
+    } else{
       //stella tipo c
       fill(255,255,100);
-      ellipse(starX,starY,2.8)
+      ellipse(starX,starY,2.8);
     }
-    
-  }
-  //finisce il contesto
-  pop();
+}
 
-  // nuovo contesto di disegno
+function drawStars(num_stars = 120) {
+  for(let i=0; i < num_stars; i++){
+    let starX = (i*37) % width + (i%3) * 5;
+    let starY = (i*73) % height + (i%7);
+
+    let random_transparecy = random (150,255);
+    let random_size = random (6,10);
+
+    drawSingleStar(i, starX, starY, random_transparecy, random_size)
+  }
+
+}
+
+function drawRocket(xRocket, yRocket) {
   push();
   fill(220);
   stroke(40);
@@ -79,16 +65,37 @@ function draw() {
   strokeWeight(2)
   triangle(xRocket-40,yRocket+90,xRocket-20,yRocket+90,xRocket-70,yRocket+120);
 
-  triangle(xRocket+40,yRocket+90,xRocket+20,yRocket+90,xRocket+70,yRocket+120)
-
+  triangle(xRocket+40,yRocket+90,xRocket+20,yRocket+90,xRocket+70,yRocket+120);
   pop();
+}
 
- // xRocket = (xRocket+1) % xMax;
-  yRocket = (yRocket-1);
-  //quando la yRocket sarà oltre una certa soglia
-  // allora dobbiamo resettarla
+
+function moveRocket(yRocket, step=1){
+  yRocket = yRocket - step;
   let soglia = -(yMax * 0.6);
-  if(yRocket < soglia) {
+  if (yRocket < soglia) {
     yRocket = yMax;
   }
+  return yRocket;
+}
+
+
+
+function draw() {
+  background(20,24,40);
+  // mostrare un testo bianco
+  // che dice le coordinate al mouse
+  //sul foglio da disegno
+  fill(255); //bianco
+  textSize(20);
+  text("mouseX: " + mouseX +  ",    mouseY: " + mouseY,20,20);  //stringa, x, y
+
+
+drawStars(100);
+
+
+drawRocket(xRocket, yRocket);
+
+yRocket = moveRocket(yRocket, step=1);
+
 }
